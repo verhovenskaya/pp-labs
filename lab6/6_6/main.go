c85ef2ea43d9532344ef4cd56b1932c2215da466
Алина <sanshinjam690@gmail.com>
2024-10-16 10:45:05 +0400

    *

--- lab6/6_6/main.go
+++ lab6/6_6/main.go
@@ -4,85 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
-	"sync"
+	"time"
 )
 
-// Задача: реверсирование строки
-type Task struct {
-	Line string
+type Job struct {
+	Content string
 }
 
-// Результат: реверсированная строка
-type Result struct {
-	Line string
-	Err  error
-}
-
-// Функция, выполняющая реверсирование строки
-func reverseString(task Task) Result {
-	reversed := strings.Split(task.Line, "")
-	for i, j := 0, len(reversed)-1; i < j; i, j = i+1, j-1 {
-		reversed[i], reversed[j] = reversed[j], reversed[i]
+func ReverseString(s string) string {
+	reversed := make([]byte, len(s))
+	for i, j := 0, len(s)-1; i < len(s); i, j = i+1, j-1 {
+		reversed[i] = s[j]
 	}
-	return Result{Line: strings.Join(reversed, ""), Err: nil}
+	return string(reversed)
 }
 
-// Функция воркера
-func worker(taskChan chan Task, resultChan chan Result, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	for task := range taskChan {
-		result := reverseString(task)
-		resultChan <- result
+func processTasks(taskChannel <-chan Job) {
+	for job := range taskChannel {
+		result := ReverseString(job.Content)
+		fmt.Println(result)
 	}
 }
 
 func main() {
-	// Получение имени файла и количества воркеров от пользователя
-	fmt.Print("Введите имя файла: ")
-	var filename string
-	fmt.Scanln(&filename)
+	var workerCount int
+	fmt.Print("Укажите количество рабочих потоков: ")
+	fmt.Scan(&workerCount)
 
-	fmt.Print("Введите количество воркеров: ")
-	var numWorkers int
-	fmt.Scanln(&numWorkers)
+	taskChannel := make(chan Job)
 
-	// Открытие файла
-	file, err := os.Open(filename)
+	for i := 0; i < workerCount; i++ {
+		go processTasks(taskChannel)
+	}
+
+	file, err := os.Open("ку.txt")
 	if err != nil {
-		fmt.Println("Ошибка открытия файла:", err)
+		fmt.Println("Не удалось открыть файл:", err)
 		return
 	}
 	defer file.Close()
 
-	// Каналы для задач и результатов
-	taskChan := make(chan Task)
-	resultChan := make(chan Result)
-
-	// Создание пула воркеров
-	var wg sync.WaitGroup
-	wg.Add(numWorkers)
-	for i := 0; i < numWorkers; i++ {
-		go worker(taskChan, resultChan, &wg)
-	}
-
-	// Чтение строк из файла и отправка задач в канал
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		taskChan <- Task{Line: scanner.Text()}
+		taskChannel <- Job{Content: scanner.Text()}
 	}
-	// Закрываем канал задач после того, как прочитали все строки из файла
-	close(taskChan)
 
-	// Получение результатов из канала и вывод в консоль
-	wg.Wait()
-	for i := 0; i < numWorkers; i++ {
-		result := <-resultChan
-		if result.Err != nil {
-			fmt.Println("Ошибка:", result.Err)
-		} else {
-			fmt.Println("Реверсированная строка:", result.Line)
-		}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Ошибка при чтении файла:", err)
 	}
+
+	close(taskChannel)
+	time.Sleep(1 * time.Second)
 }
